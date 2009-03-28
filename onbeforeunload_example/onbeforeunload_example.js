3c94feebd170f94a0d62c338c716c55221e62da6
Marc Ferran (aka Markus) <markus_petrux@39593.no-reply.drupal.org>
2009-03-28 12:36:39 +0000

    - Fix camelcase names for javascript objects.

--- onbeforeunload_example/onbeforeunload_example.js
+++ onbeforeunload_example/onbeforeunload_example.js
@@ -5,9 +5,9 @@
  *
  * This function will be called by Drupal.attachBehaviors() in misc/drupal.js.
  */
-Drupal.behaviors.onbeforeunload_example = function(context) {
+Drupal.behaviors.onBeforeUnloadExample = function(context) {
   if (!Drupal.onBeforeUnload.callbackExists('onbeforeunload_example')) {
-    Drupal.onBeforeUnload.addCallback('onbeforeunload_example', Drupal.onbeforeunload_example);
+    Drupal.onBeforeUnload.addCallback('onbeforeunload_example', Drupal.onBeforeUnloadExample);
   }
 };
 
@@ -20,7 +20,7 @@ Drupal.behaviors.onbeforeunload_example = function(context) {
  * The string returned here will be prompted to the user, so do NOT return
  * anything if you do not need to.
  */
-Drupal.onbeforeunload_example = function() {
+Drupal.onBeforeUnloadExample = function() {
   if (Drupal.settings.onBeforeUnloadExample.showWarning) {
     return 'Hello world!\n\nThis warning has been provided by the onBeforeUnload Example module.';
   }
